146020146f3fd9811f6f3ac11a89ff4250ed4bbd
FilipeSO <fsaolive@gmail.com>
2019-02-21 14:22:30 -0300

    inicio versão publica do SOS

---
+++ SOS/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+        private string LogText = "";
+
+        private void UILogUpdate(string report)
+        {
+            LogText += $"{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")}: {report}{Environment.NewLine}";
+            report = report.Length > 100 ? $"{report.Substring(0, 100)}..." : report;
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)(() =>
+                {
+                    toolStripStatusLabel1.Text = $"Atualização de documentos: {report}";
+                }));
+            }
+        }
+        private void FileLogUpdate()
+        {
+            File.WriteAllText($"{Environment.CurrentDirectory}/Documentos/log.txt", LogText);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                UpdateMPO();
+                FileLogUpdate();
+            });
+        }
+
+        private void UpdateMPO()
+        {
+            UILogUpdate("ons.org.br/ conectando...");
+            List<ChildItem> docsMPO = WebScrap.ScrapMPOAsync("FURNAS").GetAwaiter().GetResult();
+            var client = new WebClient();
+            foreach (var doc in docsMPO)
+            {
+                UILogUpdate($"{doc.MpoCodigo} atualizando");
+                string docLink = $"http://ons.org.br{doc.FileRef}";
+                FileInfo docFile = new FileInfo($"{Environment.CurrentDirectory}/Documentos/MPO/{doc.MpoCodigo}/{doc.MpoCodigo}.pdf");
+                if (!docFile.Directory.Exists) Directory.CreateDirectory(docFile.Directory.FullName);
+
+                string revisao = File.Exists($"{docFile.DirectoryName}/info.cfg") ? File.ReadAllText($"{docFile.DirectoryName}/info.cfg") : null;
+                if (revisao == doc._Revision)
+                {
+                    UILogUpdate($"{doc.MpoCodigo} já se encontra na revisão vigente");
+                    continue;
+                }
+                try
+                {
+                    client.DownloadFile(docLink, docFile.FullName);
+                    UILogUpdate($"{doc.MpoCodigo} atualizado da revisão {revisao} para revisão {doc._Revision} em {docFile.FullName}");
+                    File.WriteAllText($"{docFile.DirectoryName}/info.cfg", doc._Revision);
+                    if (doc.MpoAlteradosPelasMops != null)
+                    {
+                        File.WriteAllText($"{docFile.DirectoryName}/mop.cfg", doc.MpoAlteradosPelasMops.Replace('/', '-'));
+                        UILogUpdate($"{doc.MpoCodigo} está sendo alterado por {doc.MpoAlteradosPelasMops.Replace('/', '-')}");
+                    }
+                }
+                catch (Exception)
+                {
+                    UILogUpdate($"{doc.MpoCodigo} não foi possível atualização pelo link {docLink}");
+                }
+            }
+            List<string> mopLinks = docsMPO.Where(w => w.MpoMopsLink != null).SelectMany(s => s.MpoMopsLink).Distinct().ToList();
+            string mopDir = $"{Environment.CurrentDirectory}/Documentos/MPO/MOP";
+            if (!Directory.Exists(mopDir)) Directory.CreateDirectory(mopDir);
+            var mopsLocal = Directory.GetFiles(mopDir, "*.pdf").ToList();
+            var mopsVigentes = mopLinks.Select(s => $"{mopDir}\\{s.Split('/').Last()}").ToList();
+            mopsLocal = mopsLocal.Where(w=>!mopsVigentes.Contains(w)).ToList();
+            mopsLocal.ForEach(s => 
+            {
+                File.Delete(s);
+                UILogUpdate($"{s.Split('/').Last()} não está vigente e foi apagada");
+            });
+            foreach (var mopLink in mopLinks)
+            {
+                FileInfo mopFile = new FileInfo($"{Environment.CurrentDirectory}/Documentos/MPO/MOP/{mopLink.Split('/').Last()}");
+                UILogUpdate($"{mopFile.Name} atualizando");
+                if (mopFile.Exists)
+                {
+                    UILogUpdate($"{mopFile.Name} já está disponível em {mopFile.FullName}");
+                    continue;
+                }
+                try
+                {
+                    client.DownloadFile(mopLink, mopFile.FullName);
+                    UILogUpdate($"{mopFile.Name} disponível em {mopFile.FullName}");
+                }
+                catch (Exception)
+                {
+                    UILogUpdate($"{mopFile.Name} não foi possível atualização pelo link {mopLink}");
+                }
+            }
+        }
+
+    }
+}
---
+++ SOS/WebScrap.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SOS
+{
+    static class WebScrap
+    {
+
+        static readonly HttpClientHandler handler = new HttpClientHandler()
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+        };
+        static HttpClient client = new HttpClient(handler);
+
+        public static async Task<List<ChildItem>> ScrapMPOAsync(string agente)
+        {
+            string requestToken = await GetRequestDigestToken();
+            ModelMPO docs = await GetModelMPO(requestToken);
+            var validDocs = docs._Child_Items_.Where(w => w.MpoAgentesAssinantes != null);
+            var filteredDocs = validDocs.Where(w => w.MpoAgentesAssinantes.Contains(agente)).ToList();
+            filteredDocs = CompileMopLinks(filteredDocs);
+            return filteredDocs;
+        }
+
+        static List<ChildItem> CompileMopLinks(List<ChildItem> docs)
+        {
+            foreach (var item in docs.Where(w => w.MpoAlteradosPelasMops != null))
+            {
+                item.MpoMopsLink = new List<string>();
+                foreach (var mop in item.MpoAlteradosPelasMops.ToString().Split(',')) //formato: MOP/ONS xxx-R/2018,MOP/ONS xxx-S/2019 
+                {
+                    string tag = mop.Contains("R") == true ? "Regional" : "Sistêmica";
+                    string responsavel = item.MpoResponsavel;
+                    string fileMOP = mop.Replace('/', '-');
+                    item.MpoMopsLink.Add($"http://www.ons.org.br/MPO/Mensagem Operativa/{tag}/{responsavel}/{fileMOP}.pdf");
+                }                
+            }
+            return docs;
+        }
+
+        static async Task<string> GetRequestDigestToken()
+        {
+            var getResult = await client.GetAsync("http://ons.org.br/paginas/sobre-o-ons/procedimentos-de-rede/mpo");
+            var html = await getResult.Content.ReadAsStringAsync();
+            var requestDigestToken = Regex.Match(html, @"id=""__REQUESTDIGEST"" value=""(.*)""").Groups[1].Value;
+            return requestDigestToken;
+        }
+
+
+        static async Task<ModelMPO> GetModelMPO(string requestDigestToken)
+        {
+            client.DefaultRequestHeaders.Add("Host", "ons.org.br");
+            client.DefaultRequestHeaders.Add("X-RequestDigest", requestDigestToken);
+            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
+
+            string requestBody = @"<Request xmlns=""http://schemas.microsoft.com/sharepoint/clientquery/2009"" SchemaVersion=""15.0.0.0"" LibraryVersion=""16.0.0.0"" ApplicationName=""Javascript Library""><Actions><Query Id=""24"" ObjectPathId=""11""><Query SelectAllProperties=""true""><Properties /></Query><ChildItemQuery SelectAllProperties=""true""><Properties /></ChildItemQuery></Query></Actions><ObjectPaths><Method Id=""11"" ParentId=""8"" Name=""GetItems""><Parameters><Parameter TypeId=""{3d248d7b-fc86-40a3-aa97-02a75d69fb8a}""><Property Name=""DatesInUtc"" Type=""Boolean"">true</Property><Property Name=""FolderServerRelativeUrl"" Type=""String"">/MPO/Documento Normativo</Property><Property Name=""ListItemCollectionPosition"" Type=""Null"" /><Property Name=""ViewXml"" Type=""String"">&lt;View Scope='RecursiveAll'&gt;   &lt;Query&gt;       &lt;OrderBy&gt;       	&lt;FieldRef Name='Title' /&gt;       	&lt;FieldRef Name='MpoAssunto' /&gt;       	&lt;FieldRef Name='FileLeafRef' /&gt;   	&lt;/OrderBy&gt;   &lt;/Query&gt;&lt;/View&gt;</Property></Parameter></Parameters></Method><Method Id=""8"" ParentId=""6"" Name=""GetByTitle""><Parameters><Parameter Type=""String"">MPO</Parameter></Parameters></Method><Property Id=""6"" ParentId=""4"" Name=""Lists"" /><Property Id=""4"" ParentId=""2"" Name=""RootWeb"" /><Property Id=""2"" ParentId=""0"" Name=""Site"" /><StaticProperty Id=""0"" TypeId=""{3747adcd-a3c3-41b9-bfab-4a64dd2f1e0a}"" Name=""Current"" /></ObjectPaths></Request>";
+            var httpContent = new StringContent(requestBody, Encoding.UTF8, "text/xml");
+
+            //var resultPost = await client.PostAsync("/_vti_bin/client.svc/ProcessQuery", httpContent);
+            var resultPost = await client.PostAsync("http://ons.org.br/_vti_bin/client.svc/ProcessQuery", httpContent);
+
+            var resultContent = await resultPost.Content.ReadAsStringAsync();
+
+            string corteInicio = "\"_Child_Items_\":[";
+            resultContent = "{" + resultContent.Substring(resultContent.IndexOf(corteInicio));
+            resultContent = resultContent.Substring(0, resultContent.LastIndexOf("]") - 1).Trim();
+            ModelMPO documentos = JsonConvert.DeserializeObject<ModelMPO>(resultContent);
+            return documentos;
+        }
+
+    }
+}
